b6d2cc621139fc1c7203af1b9887c4bb811bc0ae
cilantech <isazaherbin@gmail.com>
2024-04-16 06:44:55 -0500

    contar caracteres

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,10 @@
 
 let comillaSimple='hola mundo'
 let comillasDobles="hola 'mundo'"
+let fullName = "Herbin Esteban Restrepo"
+const cantidadDeCaracteres = fullName.length
 
 
 console.info(comillaSimple)
 console.info(comillasDobles)
+console.info("la cantidad de caracteres =",cantidadDeCaracteres)
